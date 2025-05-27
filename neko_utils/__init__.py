9b23aa12d8a98db7356ac734aa11cb5d89a3fffe
Lorenzo Donati <lorenzoluca.donati@gmail.com>
2025-05-27 10:00:36 +0200

    added comp nut

--- neko_utils/__init__.py
+++ neko_utils/__init__.py
@@ -1,4 +1,4 @@
-from .neko_ds_tools import nek_dataset,NekDataStore
+from .neko_ds_tools import nek_dataset,NekDataStore,comp_nut
 from .csv_tools import csv_to_xr
 
 __all__ = ["neko_ds_tools", "nek_dataset", "csv_to_xr","comp_nut"]
